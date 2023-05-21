8287fda033101f2be95d47a6f13b7694c90af1e3
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2023-05-22 02:37:20 +0530

    Feat: Archive note

--- app.js
+++ app.js
@@ -15,10 +15,11 @@ let closeToastBtn = document.querySelector(".close-Toast");
 let addedText = document.querySelector(".addedText");
 let showNotesDiv = document.querySelector(".showNotesDiv");
 let allNotes = [];
+let archivedNotes = [];
+let archivedNotesDiv = document.querySelector(".archivedNotesDiv");
 
 // Searchbar styling starts here
 searchBarContainer.addEventListener("click", changeBackground);
-
 function changeBackground(e) {
   if (e.target.tagName === "INPUT") {
     searchBarContainer.style.background = "white";
@@ -38,7 +39,8 @@ function outsideClick(e) {
 
 archivesAppearHere.style.display = "none";
 trashedAppearHere.style.display = "none";
-// Sidebar heading bgcolor styling starts here
+archivedNotesDiv.style.display = "none";
+// Sidebar heading styling starts here
 notesHeading.classList.add("active-link");
 notesHeading.addEventListener("click", () => {
   notesHeading.classList.add("active-link");
@@ -48,6 +50,7 @@ notesHeading.addEventListener("click", () => {
   showNotesDiv.style.display = "flex";
   archivesAppearHere.style.display = "none";
   trashedAppearHere.style.display = "none";
+  archivedNotesDiv.style.display = "none";
 });
 
 archiveHeading.addEventListener("click", () => {
@@ -55,9 +58,13 @@ archiveHeading.addEventListener("click", () => {
   archiveHeading.classList.add("active-link");
   trashHeading.classList.remove("active-link");
   createNoteCard.style.display = "none";
-  archivesAppearHere.style.display = "flex";
   trashedAppearHere.style.display = "none";
   showNotesDiv.style.display = "none";
+  if (!archivedNotesDiv) {
+    archivesAppearHere.style.display = "flex";
+  } else {
+    archivedNotesDiv.style.display = "flex";
+  }
 });
 
 trashHeading.addEventListener("click", () => {
@@ -68,10 +75,11 @@ trashHeading.addEventListener("click", () => {
   archivesAppearHere.style.display = "none";
   trashedAppearHere.style.display = "flex";
   showNotesDiv.style.display = "none";
+  archivedNotesDiv.style.display = "none";
 });
-// Sidebar heading bgcolor styling ends here
+// Sidebar heading styling ends here
 
-// hide toast
+// hide toast initially
 showToastDiv.style.display = "none";
 addNoteBtn.addEventListener("click", saveNote);
 function saveNote() {
@@ -90,9 +98,9 @@ function saveNote() {
     allNotes.push(allNotesObj);
     console.log(allNotes);
     renderAllnotes();
+    addTitle.value = "";
+    addNote.value = "";
   }
-  addTitle.value = "";
-  addNote.value = "";
 }
 
 closeToastBtn.addEventListener("click", () => {
@@ -106,11 +114,13 @@ function renderAllnotes() {
   <h1 class="addedTitle">${note.providedTitle}</h1>
   <p class="addedText">${note.providedNote}</p>
   <div class="operation-icons">
-    <button class="btn btn-primary btn-floating btn-add-note">
+    <button class="btn btn-primary btn-floating btn-archive-note">
+    <span class="archive-icon">
       <svg
+        class="svg-archive-note"
         xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
+        width="22"
+        height="22"
         viewBox="0 0 24 24"
       >
         <path
@@ -118,12 +128,15 @@ function renderAllnotes() {
           d="M5 21q-.825 0-1.413-.588T3 19V6.5q0-.375.125-.675t.325-.575l1.4-1.7q.2-.275.5-.413T6 3h12q.35 0 .65.137t.5.413l1.4 1.7q.2.275.325.575T21 6.5V19q0 .825-.588 1.413T19 21H5Zm.4-15h13.2l-.85-1H6.25L5.4 6ZM5 8v11h14V8H5Zm7 10l4-4l-1.4-1.4l-1.6 1.6V10h-2v4.2l-1.6-1.6L8 14l4 4Zm-7 1h14H5Z"
         />
       </svg>
+      </span>
     </button>
-    <button class="btn btn-primary btn-floating btn-add-note">
-      <svg
+    <button class="btn btn-primary btn-floating btn-trash-note">
+    <span class="trash-icon">  
+    <svg
+      class="svg-trash-note"
         xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
+        width="22"
+        height="22"
         viewBox="0 0 24 24"
       >
         <path
@@ -131,9 +144,61 @@ function renderAllnotes() {
           d="M7 21q-.825 0-1.413-.588T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.588 1.413T17 21H7ZM17 6H7v13h10V6ZM9 17h2V8H9v9Zm4 0h2V8h-2v9ZM7 6v13V6Z"
         />
       </svg>
+      </span>
     </button>
   </div>
 </div>`;
   }
   showNotesDiv.innerHTML = notes;
+  let btnArchiveNote = document.querySelectorAll(".btn-archive-note");
+  console.log(btnArchiveNote);
+
+  btnArchiveNote.forEach((item) => {
+    item.addEventListener("click", handler);
+  });
+  function handler(event) {
+    let noteCardArr = Array.from(showNotesDiv.children);
+    let indexOfNoteCardToBeArchived = noteCardArr.indexOf(
+      event.target.closest(".noteCard")
+    );
+    let archivedNote = noteCardArr.splice(indexOfNoteCardToBeArchived, 1)[0];
+    console.log(archivedNote);
+    archivedNotes.push(archivedNote);
+    allNotes.splice(indexOfNoteCardToBeArchived, 1);
+    renderAllnotes();
+    renderArchivedNotes();
+  }
+}
+
+function renderArchivedNotes() {
+  let archivedNotesHTML = "";
+  for (note of archivedNotes) {
+    archivedNotesHTML += `
+      <div class="noteCard">
+        <h1 class="addedTitle">${
+          note.querySelector(".addedTitle").textContent
+        }</h1>
+        <p class="addedText">${note.querySelector(".addedText").textContent}</p>
+        <div class="operation-icons">
+        <button class="btn btn-primary btn-floating btn-trash-note">
+        <span class="trash-icon">  
+        <svg
+      class="svg-trash-note"
+        xmlns="http://www.w3.org/2000/svg"
+        width="22"
+        height="22"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M7 21q-.825 0-1.413-.588T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.588 1.413T17 21H7ZM17 6H7v13h10V6ZM9 17h2V8H9v9Zm4 0h2V8h-2v9ZM7 6v13V6Z"
+        />
+      </svg>
+      </span>
+    </button>
+    </div>
+      </div>
+    `;
+  }
+  archivedNotesDiv.innerHTML = archivedNotesHTML;
 }
